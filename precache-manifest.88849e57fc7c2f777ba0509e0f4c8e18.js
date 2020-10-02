72e2156d7dda61449c8c03020a64eb06e58509f5
Ashwani <av11000100@gmail.com>
2020-10-02 18:02:50 +0530

    Updates

---
+++ precache-manifest.88849e57fc7c2f777ba0509e0f4c8e18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a692b00e69dfddbc409b8ccf2ec0137d",
+    "url": "/2do/index.html"
+  },
+  {
+    "revision": "bceff590aa0993992f8d",
+    "url": "/2do/static/css/main.4f0d9408.chunk.css"
+  },
+  {
+    "revision": "085c04c54aa72d43ea28",
+    "url": "/2do/static/js/2.5ea8ae43.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/2do/static/js/2.5ea8ae43.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bceff590aa0993992f8d",
+    "url": "/2do/static/js/main.bcf22a50.chunk.js"
+  },
+  {
+    "revision": "7f781e9b9c971a7a5d42",
+    "url": "/2do/static/js/runtime-main.49ce45af.js"
+  }
+]);
